6a0eeda1344c792c3191282a7a10853ba06dbdfb
wolak88 <wolak88@gmail.com>
2016-05-16 11:03:49 +0200

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'welcome_page/welcome'
+  root 'welcome_page#welcome'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
